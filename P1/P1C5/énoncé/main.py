9bcbd025316ab0e71dae3ba9bf4be7a7136defbc
Yanick-Romuald <59802682+Yanick-Romuald@users.noreply.github.com>
2026-07-14 17:13:05 -0700

    Initialize variables and print their types
    Added variables for name, age, height, and student status with type checks.

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,13 @@
 # Écrivez votre code ici !
+
+nom = "YaRo"
+age = 25
+taille = 1.85
+est_etudiant = True
+
+typeNom = type(nom)
+typeAge= type(age)
+typeTaille = type(taille)
+typeEstEtudiant = type(est_etudiant)
+
+print(f"Mon nom est {nom} qui est de type {typeNom}, j'ai {age} ans qui est de type {typeAge}, je fais {taille} cm qui est de type {typeTaille} et c'est {est_etudiant} que je suis étudiant qui est une variable de type {typeEstEtudiant}")
