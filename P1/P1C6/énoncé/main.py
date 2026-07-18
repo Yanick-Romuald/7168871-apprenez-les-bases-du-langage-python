308038e2eb1d545e6df84efa35ae7783bc500263
Yanick-Romuald <59802682+Yanick-Romuald@users.noreply.github.com>
2026-07-18 13:16:24 -0700

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,16 @@
 # Écrivez votre code ici !
+
+
+fruits = ["pomme", "banane", "orange"]
+print(f"La liste initiale de fruits est : {fruits}")
+
+fruits.append("kiwi")
+print(f"La liste avec un élément ajouté est : {fruits}")
+
+fruits[1]= "ananas"
+print(f"La liste avec un élément de la liste modifié est : {fruits}")
+
+print(f"La taille de la liste est : {len(fruits)}")
+
+fruits.sort()
+print(f"La liste triée est : {fruits}")
