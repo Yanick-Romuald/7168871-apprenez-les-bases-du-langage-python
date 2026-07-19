d727da7a5001d887cd32311f1ace5f1e1a7dd904
Yanick-Romuald <59802682+Yanick-Romuald@users.noreply.github.com>
2026-07-19 10:23:28 -0700

    Implement fruit dictionary operations in main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,25 @@
 # Écrivez votre code ici !
+
+
+
+
+fruits = {"pomme":"rouge", "banane":"jaune", "orange":"orange"}
+print(f"Le dictionnaire de fruits est : {fruits}")
+fruits.items()
+
+fruits['kiwi']="vert"
+print(f"Le nouveau dictionnaire avec un élément de plus est : {fruits} ")
+
+couleur_banane = fruits.get("banane")
+print("La couleur de la banane est : {couleur_banane}")
+
+fruits= {"pomme" : "vert"}
+
+print(f"la nouvelle liste de fruits est : {fruits}")
+
+del fruits["banane"]
+
+print(f"la nouvelle liste de fruits est : {fruits}")
+
+print(f"La liste des clés est la suivante : {fruits.keys()}
+
