06f69c01315be856b0130c86cf0a6917f7f068af
Yanick-Romuald <59802682+Yanick-Romuald@users.noreply.github.com>
2026-07-21 12:41:42 -0700

    Add salary calculation functions in main.py

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,1 +1,18 @@
 # Ecrivez votre code ici
+def salaire_mensuel(salaire_annuel) :
+    saMe = salaire_annuel / 12
+    return saMe
+
+def salaire_hebdomadaire(salaire_mensuel) :
+    saHe = salaire_mensuel / 4
+    return saHe
+
+def salaire_horaire(salire_hebdomadaire, heures_travaillees) :
+    saHo = salire_hebdomadaire / heures_travaillees
+    return saHo
+
+salaire_annuel=float(input("Entrez votre salaire annuel : "))
+
+heuresTravaillees = int(input("Entrez le nombre d'heures travaillées par semaine : "))
+
+print(f"Votre salaire horaire est de : {round(salaire_horaire(salaire_hebdomadaire(salaire_mensuel(salaire_annuel)), heuresTravaillees),2)} euros")
