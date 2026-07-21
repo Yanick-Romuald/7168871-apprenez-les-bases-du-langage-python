dce9c84fc2f7b0eff9081c39933ce1b277b42d9f
Yanick-Romuald <59802682+Yanick-Romuald@users.noreply.github.com>
2026-07-21 12:05:41 -0700

    Implement sum calculation for user-input numbers
    Add functionality to sum a list of numbers from user input.

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,13 @@
 # Ecrivez votre code ici !
+
+nombres = input("Entrez votre liste de nombre séparés par des virgules")
+liste = nombres.strip().split(",")
+liste_entiers = []
+somme = 0
+for i in liste :
+  liste_entiers.append(int(i))
+
+for i in liste_entiers :
+  somme += i
+
+print(f"La somme des nombres entrés est {somme}")
