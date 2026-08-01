dee4ae8314328489224f9ca3db92ee84333f5e07
Yanick-Romuald <59802682+Yanick-Romuald@users.noreply.github.com>
2026-07-31 17:03:33 -0700

    Add operations for addition and multiplication

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,9 @@
 # Ecrivez votre code ici !
+import operations
+nombre1=3
+nombre2=5
+  print(f"La somme de {nombre1} et {nombre2} est {operations.addition(nombre1, nombre2)} ")
+  print(f"La produit de {nombre1} et {nombre2} est {operations.multiplication(nombre1, nombre2)} ")
+
+
+
